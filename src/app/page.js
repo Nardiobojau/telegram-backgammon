ca208e6d03412f9a6e76403f56e65d7c111a28de
Nardiobojau <vitaliykov13@gmail.com>
2025-02-06 13:48:04 +0500

    page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import "../../styles.css";
+import "../styles.css";
 
 export default function Home() {
   const [user, setUser] = useState(null);
